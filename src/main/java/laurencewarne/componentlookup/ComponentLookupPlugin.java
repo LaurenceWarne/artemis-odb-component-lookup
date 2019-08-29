67189095f5f472223a44da8ea3085842d38cf152
lWarne <laurencewarne@gmail.com>
2019-08-29 10:53:08 +0100

    Initial commit.

---
+++ src/main/java/laurencewarne/componentlookup/ComponentLookupPlugin.java
@@ -0,0 +1,12 @@
+package laurencewarne.componentlookup;
+
+import com.artemis.ArtemisPlugin;
+import com.artemis.WorldConfigurationBuilder;
+
+public class ComponentLookupPlugin implements ArtemisPlugin {
+
+    @Override
+    public void setup(final WorldConfigurationBuilder b) {
+	
+    }
+}
